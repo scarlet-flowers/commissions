510c948d40477738d627bd8f56e353ac9038d936
theyoostink <90541264+theyoostink@users.noreply.github.com>
2026-06-09 17:11:47 -0400

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,28 @@
+var data = {
+	images: [
+		{
+			src: [""],
+			thumbnail: "",
+			title: "Placeholder Entry",
+			artist: "Placeholder Entry",
+			artist_url: null,
+			art_url: null,
+			desc: "Do not delete this. You can use this to set the order the tags show up in the dropdown.",
+			date_str: "Beginning of Time",
+			tags: [],
+			hidden: true
+		},
+		{
+			src: [""],
+			thumbnail: "",
+			title: "",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: [""],
+			hidden: true
+		}
+	]
+};
