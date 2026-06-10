048cb88c27611224cd7eeab9bfcaf4136a5f6e6f
scarlet-flowers <5c4rletfl0w3r5@gmail.com>
2026-06-10 01:56:06 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -13,6 +13,30 @@ var data = {
 			hidden: true
 		},
 		{
+			src: ["https://cdn.imgchest.com/files/183f4a393132.png"],
+			thumbnail: "https://cdn.imgchest.com/files/c91c92b1dc63.png",
+			title: "Great Lord Lucina and Bow Knight Inigo",
+			artist: "Lace",
+			artist_url: "https://bsky.app/profile/adelleein.bsky.social",
+			art_url: "null",
+			desc: "One of the very first commissions I ever got, which was done for a donation rally. The commission depicts my preferred final classes for Lucina and Inigo, which is Great Lord and Bow Knight respectively. The flower petals around Lucina are a reference to the skill Ignis. The bow sprite is Dignified Bow, and the sword sprite is Skuld.",
+			date_str: "August 2020",
+			tags: ["fire emblem", "lucina", "inigo", "lucinigo", "adelleein"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/08fa2ad5fa25.png"],
+			thumbnail: "https://cdn.imgchest.com/files/cff158f2e7c2.png",
+			title: "Grandmaster Morgan and Dark Flier Severa",
+			artist: "Lace",
+			artist_url: "https://bsky.app/profile/adelleein.bsky.social",
+			art_url: "null",
+			desc: "One of the very first commissions I ever got, which was done for a donation rally. The commission depicts my preferred final classes for Morgan and Severa, which is Grandmaster and Dark Flier respectively. The green tome sprite is Excalibur.",
+			date_str: "August 2020",
+			tags: ["fire emblem", "severa", "male morgan", "sevimorgan", "adelleein"],
+			hidden: false
+		},		
+		{
 			src: [""],
 			thumbnail: "",
 			title: "",
