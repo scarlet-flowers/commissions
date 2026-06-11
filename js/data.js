8ff9a0cf57ffae73cf95f30d5683632a122b0f98
scarlet-flowers <5c4rletfl0w3r5@gmail.com>
2026-06-11 01:12:43 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -182,7 +182,7 @@ var data = {
 			title: "Family Outing",			
 			artist: "Siren",
 			artist_url: "https://x.com/ladylawga",
-			desc: "A birthday gift from my friend, Silent. While Chrom and Robin have enjoying their moment of peace, Morgan is causing a bit of chaos for his sister in the background.",
+			desc: "A birthday gift from my friend, Silent. While Chrom and Robin are enjoying their moment of peace, Morgan is causing a bit of chaos for his sister in the background.",
 			date_str: "September 2022",
 			tags: ["Fire Emblem", "Chrom", "Female Robin", "Lucina", "Male Morgan", "Exalt Family", "Gift"],
 			hidden: false
